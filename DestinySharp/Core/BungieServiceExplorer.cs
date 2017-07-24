c1d7654334fdf971f65ab2248d35d82efc24697e
Fires1 <samuelcadams02@gmail.com>
2017-07-24 13:08:13 -0500

    main commit

---
+++ DestinySharp/Core/BungieServiceExplorer.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DestinySharp.Core
+{
+    public class DestinyServiceExplorer
+    {
+        RestClient _client = new RestClient("http://www.bungie.net/Platform/Destiny");
+        string apikey { get; set; }
+
+        public Data GetCharacterSummary(string name, MembershipType type)
+        {
+            RestRequest request = new RestRequest($"/{(int)type}/Account/{GetMembershipId(name, type)}/Summary/");
+            request.AddHeader("X-API-KEY", apikey);
+
+            IRestResponse response = _client.Execute(request);
+
+            var r = JsonConvert.DeserializeObject<DestinyServiceObjectResponse>(response.Content);
+
+            return r.Response.data;
+        }
+
+        internal string GetMembershipId(string name, MembershipType type)
+        {
+            RestRequest request = new RestRequest($"/{(int)type}/Stats/GetMembershipIdByDisplayName/{name}/");
+            request.AddHeader("X-API-KEY", apikey);
+            IRestResponse<DestinyServiceResponse> response = _client.Execute<DestinyServiceResponse>(request);
+
+            return response.Data.Response;
+        }
+
+        public DestinyServiceExplorer(string apikey)
+        {
+            this.apikey = apikey;
+        }
+    }
+}
---
+++ DestinySharp/Core/Enum/MembershipType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DestinySharp.Core
+{
+    /// <summary>
+    /// Membershiptype that Bungie api takes
+    /// </summary>
+    public enum MembershipType
+    {
+        /// <summary> Used for xbox gamers </summary>
+        Xbox = 1,
+        /// <summary> Used for psn gamers </summary>
+        PSN = 2,
+        /// <summary> WARNING: NO IDEA IF THIS WORKS </summary>
+        Blizzard = 4,
+    }
+}
