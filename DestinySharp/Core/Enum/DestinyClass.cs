07d2ae37e345d74dd7d35668f9ccf200893f0d06
Fires1 <samuelcadams02@gmail.com>
2017-07-24 22:26:21 -0500

    Useful Enumerations

---
+++ DestinySharp/Core/Enum/DestinyClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DestinySharp.Core
+{
+    public enum DestinyClass
+    {
+        Titan,
+        Hunter,
+        Warlock
+    }
+}
--- DestinySharp/Core/Enum/DestinyDefinitionType.cs
+++ DestinySharp/Core/Enum/DestinyDefinitionType.cs
@@ -4,9 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DestinySharp.Core.Enum
+namespace DestinySharp.Core
 {
-    class DestinyDefinitionType
+    public enum DestinyDefinitionType
     {
+        Activity,
+        ActivityType,
+        Class,
+        Gender,
+        InventoryBucket,
+        InventoryItem,
+        Progression,
+        Race,
+        Stat,
+        TalentGrid,
+        StatGroup,
+        UnlockFlag,
+        Vendor,
+        Destination,
+        Place,
+        DirectorBook,
+        MaterialRequirement,
+        SandboxPerk,
+        ArtDye,
+        ArtDyeChannel,
+        ActivityBundle,
+        GearAsset,
+        GrimoireCard
     }
 }
