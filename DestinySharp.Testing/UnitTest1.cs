b34013623825ba8d5f0ce0a2969213a54f4280ff
Fires1 <samuelcadams02@gmail.com>
2017-07-25 18:48:55 -0500

    Cleaned up unit testing

--- DestinySharp.Testing/UnitTest1.cs
+++ DestinySharp.Testing/UnitTest1.cs
@@ -8,20 +8,41 @@ namespace DestinySharp.Testing
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        DestinyServiceExplorer _explorer = new DestinyServiceExplorer("token");
+        private TestContext testContextInstance;
+        public TestContext TestContext
         {
-            DestinyServiceExplorer _explorer = new DestinyServiceExplorer("token");
-            //var hash = _explorer.GetCharacterSummary("Slackr4life24", MembershipType.Xbox).characters[0].characterBase.classHash;
-            //ClassDefinition d = _explorer.QueryManifest<ClassDefinition>(hash, DestinyDefinitionType.Class);
-            //Console.WriteLine(d);
-            //string s = _explorer.ItemList;
-            //Console.WriteLine(s);
-
-           // var hash = _explorer.GetCharacterSummary("Slackr4life24", MembershipType.Xbox).characters[0].characterBase.peerView.equipment[3].itemHash;
-            string d = _explorer.QueryManifestTest(14239492, DestinyDefinitionType.InventoryBucket);
-            Console.WriteLine(d);
-            Console.ReadLine();
+            get { return testContextInstance; }
+            set { testContextInstance = value; }
         }
+        [TestMethod]
+        public void ClassDefinitionManifest()
+        {
+            var hash = _explorer.GetCharacterSummary("Slackr4life24", MembershipType.Xbox).characters[0].characterBase.classHash;
+            ClassDefinition def = _explorer.QueryManifest<ClassDefinition>(hash, DestinyDefinitionType.Class);
+            Console.WriteLine(def.className);
+
+            //Hash Exploration
+            //var chara = _explorer.GetCharacterSummary("Slackr4life24", MembershipType.Xbox);
+            //string d = _explorer.QueryManifestTest(243968262, DestinyDefinitionType.);
+            //Console.WriteLine(d);
+        }
+
+        [TestMethod]
+        public void GetActivityStats()
+        {
+            var chara = _explorer.GetCharacterSummary("Slackr4life24", MembershipType.Xbox);
+            var stats = _explorer.GetActivityHistoryStats(chara.characters[0].characterBase.membershipId, chara.characters[0].characterBase.characterId, MembershipType.Xbox, DestinyActivityMode.Story, definitions: true);
+            Console.WriteLine(stats.activities[0].activityDetails.isPrivate);
+        }
+
+        [TestMethod]
+        public void GetAdviserInfo()
+        {
+            var chara = _explorer.GetCharacterSummary("Slackr4life24", MembershipType.Xbox);
+            var adviserinfo = _explorer.GetAdvisorData(chara.membershipId, MembershipType.Xbox);
+            Console.WriteLine(adviserinfo.nextDailyReset);
+        }
+
     }
 }
