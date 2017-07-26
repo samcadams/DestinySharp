6cb2bf695ff15d4f5de979316e253d1a450fe4ff
Fires1 <samuelcadams02@gmail.com>
2017-07-26 14:37:34 -0500

    Tier type enum

---
+++ DestinySharp/src/Core/Enum/TierType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DestinySharp.Core
+{
+    public enum TierType
+    {
+        Unknown,
+        Basic,
+        Common,
+        Uncommon,
+        Rare,
+        Superior,
+        Exotic
+    }
+}
