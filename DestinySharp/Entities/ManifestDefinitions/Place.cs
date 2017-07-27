d180f458f6d4fac34f70534ff792bfde1b9f9d35
Fires1 <samuelcadams02@gmail.com>
2017-07-27 15:27:04 -0500

    Place

---
+++ DestinySharp/Entities/ManifestDefinitions/Place.cs
@@ -0,0 +1,10 @@
+﻿namespace DestinySharp.Core.Entities
+{
+    public class Place : ManifestDefinition
+    {
+        public ulong PlaceHash { get; set; }
+        public string PlaceName { get; set; }
+        public string PlaceDescription { get; set; }
+        public string Icon { get; set; }
+    }
+}
