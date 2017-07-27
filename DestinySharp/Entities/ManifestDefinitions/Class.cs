7d8340252894b911aef64cb657c629d354ce78cd
Fires1 <samuelcadams02@gmail.com>
2017-07-27 14:48:13 -0500

    Class

---
+++ DestinySharp/Entities/ManifestDefinitions/Class.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DestinySharp.Core.Entities
+{
+    public class Class : ManifestDefinition
+    {
+        public ulong ClassHash { get; set; }
+        public ClassType ClassType { get; set; }
+        public string ClassName { get; set; }
+        public string ClassNameMale { get; set; }
+        public string ClassNameFemale { get; set; }
+        public string ClassIdentifier { get; set; }
+        public string MentorVendorIdentifier { get; set; }
+    }
+}
