cd5f95c50dc93f261e492757844ecd5f3051b8e2
Fires1 <samuelcadams02@gmail.com>
2017-07-27 16:39:17 -0500

    Inventory Item

---
+++ DestinySharp/Enum/SpecialItemType.cs
@@ -0,0 +1,23 @@
+﻿namespace DestinySharp.Core.Entities
+{
+    public enum SpecialItemType
+    {
+        None,
+        SpecialCurrency,
+        CompletedBounty,
+        CrucibleBounty,
+        VanguardBounty,
+        IronBannerBounty,
+        QueenBounty,
+        ExoticBounty,
+        Armor,
+        Weapon,
+        Engram = 23,
+        Consumable = 24,
+        ExchangeMaterial = 25,
+        PvpTicket = 26,
+        MissionReward = 27,
+        BountyReward = 28,
+        Currency = 29
+    }
+}
