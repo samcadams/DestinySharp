039215d25ce063af0314a51c43d6852b934dbcf6
Fires1 <samuelcadams02@gmail.com>
2017-07-27 15:41:30 -0500

    Enemy Race

---
+++ DestinySharp/Entities/ManifestDefinitions/EnemyRace.cs
@@ -0,0 +1,11 @@
+﻿namespace DestinySharp.Core.Entities
+{
+    public class EnemyRace : ManifestDefinition
+    {
+        public ulong RaceHash { get; set; }
+        public string Identifier { get; set; }
+        public string RaceName { get; set; }
+        public string Description { get; set; }
+        public string IconPath { get; set; }
+    }
+}
