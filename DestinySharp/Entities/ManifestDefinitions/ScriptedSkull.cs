4568d231f4bec4fd6ea8cb0bbfb4ccbf2babab5e
Fires1 <samuelcadams02@gmail.com>
2017-07-27 15:47:34 -0500

    Scripted Skull

---
+++ DestinySharp/Entities/ManifestDefinitions/ScriptedSkull.cs
@@ -0,0 +1,11 @@
+﻿namespace DestinySharp.Core.Entities
+{
+    class ScriptedSkull : ManifestDefinition
+    {
+        public ulong SkullHash { get; set; }
+        public string Identifier { get; set; }
+        public string SkullName { get; set; }
+        public string Description { get; set; }
+        public string IconPath { get; set; }
+    }
+}
