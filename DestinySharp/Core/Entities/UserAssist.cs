4488cc9119ff5c01297ec9c1e3e6dd2f52f31c0e
Fires1 <samuelcadams02@gmail.com>
2017-07-26 23:04:24 -0500

    change to .net framework

---
+++ DestinySharp/Core/Entities/UserAssist.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DestinySharp.Core
+{
+    public class UserAssist
+    {
+        public string Name { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public string MemberShipId { get; set; }
+        public List<ulong> CharacterIds { get; set; }
+    }
+}
