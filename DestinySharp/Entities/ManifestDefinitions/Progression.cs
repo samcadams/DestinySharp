15c5b3ca79f67c0a7b49a579f945ff08b5e108c1
Fires1 <samuelcadams02@gmail.com>
2017-07-28 09:09:07 -0500

    Progress

---
+++ DestinySharp/Entities/ManifestDefinitions/Progression.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace DestinySharp.Core.Entities
+{
+    public class Progression : ManifestDefinition
+    {
+        public ulong ProgressionHash { get; set; }
+        public string Name { get; set; }
+        public int Scope { get; set; }
+        public bool RepeatLastStep { get; set; }
+        public string Icon { get; set; }
+        public List<ProgressionStep> Steps { get; set; }
+        public bool Visible { get; set; }
+    }
+    
+    public class ProgressionStep
+    {
+        public int ProgressTotal { get; set; }
+        public List<RewardItem> RewardItems { get; set; }
+    }
+}
