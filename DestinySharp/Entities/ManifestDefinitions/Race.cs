deb7d0c448d9e5f17b44d0f554dbcef75292850a
Fires1 <samuelcadams02@gmail.com>
2017-07-28 09:09:10 -0500

    Race

---
+++ DestinySharp/Entities/ManifestDefinitions/Race.cs
@@ -0,0 +1,12 @@
+﻿namespace DestinySharp.Core.Entities
+{ 
+    public class Race : ManifestDefinition
+    {
+        public ulong RaceHash { get; set; }
+        public RaceType RaceType { get; set; }
+        public string RaceName { get; set; }
+        public string RaceNameMale { get; set; }
+        public string RaceNameFemale { get; set; }
+        public string RaceDescription { get; set; }
+    }
+}
