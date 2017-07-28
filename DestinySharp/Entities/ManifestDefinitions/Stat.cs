3d416dc4c4f756968eb373ec2cff7586eb23eea1
Fires1 <samuelcadams02@gmail.com>
2017-07-28 09:14:34 -0500

    stat

--- DestinySharp/Entities/ManifestDefinitions/Stat.cs
+++ DestinySharp/Entities/ManifestDefinitions/Stat.cs
@@ -1,6 +1,14 @@
 ﻿namespace DestinySharp.Core.Entities
 {
-    public class Stat
+    public class Stat : ManifestDefinition
     {
+        public ulong StatHash { get; set; }
+        public string StatName { get; set; }
+        public string StatDescription { get; set; }
+        public string Icon { get; set; }
+        public string StatIdentifier { get; set; }
+        public AggregationType AggregationType { get; set; }
+        public bool HasComputedBlock { get; set; }
+        public bool Interpolate { get; set; }
     }
 }
