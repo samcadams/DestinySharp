7ff755978d13fda1e7f18cb4deb77f3e275331fe
ulloav <ulloav@VAL5306054>
2017-09-05 08:01:44 -0700

    Added Test Project for DestinySharpRewritten
    will test endpoints

---
+++ DestinySharpRewritten/DestinySharpRewritten.Test/BungieClientTest/UserTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DestinySharpRewritten.Test
+{
+    [TestClass]
+    public class UserTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ DestinySharpRewritten/DestinySharpRewritten.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DestinySharpRewritten.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Boston Scientific Corporation")]
+[assembly: AssemblyProduct("DestinySharpRewritten.Test")]
+[assembly: AssemblyCopyright("Copyright © Boston Scientific Corporation 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("19cae144-88c5-422d-990a-64323d1ea8a4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
